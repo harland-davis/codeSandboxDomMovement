5de0ebbd5ee8bbe1dcfd2478e32da658548d3469
Kyle Davis <69321748+harland-davis@users.noreply.github.com>
2021-06-26 14:06:27 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import ScotchInfoBar from "./ScotchInfoBar";
+import "./styles.css";
+
+function App() {
+  const [offsetTop, setOffsetTop] = useState(300);
+
+  function moveBoxUp() {
+    setOffsetTop(offsetTop - 50);
+  }
+
+  return (
+    <div className="App">
+      <h1>Move the Box!</h1>
+
+      {/* handle the click event on this button */}
+      <button onClick={moveBoxUp}>👆 Move Up 👆</button>
+
+      {/* move this box using inline styles */}
+      <div
+        className="box"
+        style={{
+          transform: `translateY(${offsetTop}px)`
+        }}
+      />
+
+      <ScotchInfoBar />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
